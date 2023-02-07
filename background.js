47f5ae414c6193d78abe245cd28590619e4d6496
alexnancesonos <alex.nance@sonos.com>
2023-02-07 08:47:12 -0600

    fix: quick fix

--- background.js
+++ background.js
@@ -94,7 +94,7 @@ chrome.runtime.onMessage.addListener( function ( message, sender, sendResponse )
         fetch( `https://api.openai.com/v1/completions`, {
           method: "POST",
           headers: {
-            'Authorization': `Bearer ${ gptAPIKey ?? atob( gptKey ) }`,
+            'Authorization': `Bearer ${ gptAPIKey }`,
             'Content-Type': 'application/json'
           },
           body: JSON.stringify( body )
